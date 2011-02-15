cc820a5783cb51e1285a634dc4119dcf084ea9c3
Robert Sese <robert@jaguardesignstudio.com>
2011-02-15 13:21:04 -0800

    Fixing header signup link, added logo helper, and added link tests.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
 
+  # Return the app logo linked to the home page
+  def logo
+    l = image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+
   # Return a title on a per-page basis.
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
